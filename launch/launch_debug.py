9bf71cba9d68dcedfd9ff15038ece9dc679c0704
salvatore.greco <greco.salvatore03@gmail.com>
2026-04-18 16:49:01 +0000

    info: added launch_debug.py to launch node with gdbserver

---
+++ launch/launch_debug.py
@@ -0,0 +1,31 @@
+from launch import LaunchDescription
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
+
+def generate_launch_description():
+    container = ComposableNodeContainer(
+        name = 'depth_cone_map_node_container',
+        namespace = '',
+        package='rclcpp_components',
+        executable='component_container',
+        composable_node_descriptions=[
+            ComposableNode(
+                package='depth_cone_map',
+                plugin='DepthConeMapNode',
+                name='DepthConeMapNode',
+                remappings=[
+                    ("/bounding_boxes", "/cone_detection/output"),
+                    ("/depth_images", "/zed/zed_node/depth/depth_registered"),
+                    ("/camera_pose", "/orb_slam3/camera_pose"),
+                    ("/cones", "/slam/cone_map")
+                ],
+                parameters= [{
+                    "world_frame": "map",
+                    "camera_frame": "zed_camera_link"
+                }]
+            )
+        ],
+        output = 'both',
+        prefix = 'gdbserver localhost:3000'
+    )
+    return LaunchDescription([container])
