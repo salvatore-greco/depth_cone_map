9a94d360e86cf868bd5cf70ce451f67de7127290
salvatore-greco <greco.salvatore03@gmail.com>
2026-04-28 17:11:50 +0000

    wip: superpoint feature extractor

--- include/depth_cone_map/SuperPointFeatureExtractor.hpp
+++ include/depth_cone_map/SuperPointFeatureExtractor.hpp
@@ -6,22 +6,22 @@
 #include <memory>
 #include <string>
 #include <Eigen/Core>
+#include <rclcpp/logger.hpp>
 
 class SuperPointFeatureExtractor : public AbstractFeatureExtractor<Eigen::Matrix<double, 259, Eigen::Dynamic>>{
     public:
-    SuperPointFeatureExtractor(const std::string& config_path, const std::string& weight_path): 
-        superpoint_config(config_path, weight_path), 
-        superpoint(std::make_unique<SuperPoint>(superpoint_config.superpoint_config))
-        {};
+    SuperPointFeatureExtractor(const std::string& config_path, const std::string& weight_path, rclcpp::Logger logger);
     
     Eigen::Matrix<double, 259, Eigen::Dynamic> getFeatureInBB(cv::Mat image, const std::list<std::pair<cv::Point, cv::Point>>& bb) override;
+
     protected:
-    
     Eigen::Matrix<double, 259, Eigen::Dynamic> extractFeature(cv::Mat image) override;
     
     private:
     Configs superpoint_config;
     std::unique_ptr<SuperPoint> superpoint;
+
+    rclcpp::Logger logger;
 };
 
 #endif // SUPERPOINTFEATUREEXTRACTOR_HPP
--- src/SuperPointFeatureExtractor.cpp
+++ src/SuperPointFeatureExtractor.cpp
@@ -1,2 +1,25 @@
 #include "depth_cone_map/SuperPointFeatureExtractor.hpp"
+#include <rclcpp/logging.hpp>
+
+SuperPointFeatureExtractor::SuperPointFeatureExtractor(const std::string& config_path, const std::string& weight_path, rclcpp::Logger logger): 
+        superpoint_config(config_path, weight_path), 
+        superpoint(std::make_unique<SuperPoint>(superpoint_config.superpoint_config)),
+        logger(logger)
+        {
+            RCLCPP_INFO(logger, "Building engine for superpoint, may take a while if first time");
+            superpoint->build();
+        };
+
+Eigen::Matrix<double, 259, Eigen::Dynamic> SuperPointFeatureExtractor::getFeatureInBB(cv::Mat image, const std::list<std::pair<cv::Point, cv::Point>>& bb) {
+    auto feature = extractFeature(image);
+    //do something
+    return feature;
+}
+
+Eigen::Matrix<double, 259, Eigen::Dynamic> SuperPointFeatureExtractor::extractFeature(cv::Mat image) {
+    Eigen::Matrix<double, 259, Eigen::Dynamic> features;
+    superpoint->infer(image, features);
+    return features;
+}
+
 
