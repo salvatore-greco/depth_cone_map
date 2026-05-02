82fbd1c808354374c80e9a43bdc8bcceaead37e2
salvatore.greco <greco.salvatore03@gmail.com>
2026-05-02 10:37:56 +0000

    info: added left image topic to launch files

--- launch/launch_foxglove.py
+++ launch/launch_foxglove.py
@@ -1,49 +1,46 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch_ros.actions import ComposableNodeContainer
-from launch_ros.descriptions import ComposableNode
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
-import os
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import ComposableNodeContainer
+from launch_ros.descriptions import ComposableNode
 
 
 def generate_launch_description():
-    debug = DeclareLaunchArgument(
-        "debug",
-        default_value='False'
-    )
-    
+    debug = DeclareLaunchArgument("debug", default_value="False")
+
     config = os.path.join(
-        get_package_share_directory('depth_cone_map'),
-        'config',
-        'parameters.yaml'
+        get_package_share_directory("depth_cone_map"), "config", "parameters.yaml"
     )
     container = ComposableNodeContainer(
-        name = 'depth_cone_map_node_container',
-        namespace = '',
-        package='rclcpp_components',
-        executable='component_container_mt',
+        name="depth_cone_map_node_container",
+        namespace="",
+        package="rclcpp_components",
+        executable="component_container_mt",
         composable_node_descriptions=[
             ComposableNode(
-                package='depth_cone_map',
-                plugin='DepthConeMapNode',
-                name='DepthConeMapNode',
+                package="depth_cone_map",
+                plugin="DepthConeMapNode",
+                name="DepthConeMapNode",
                 remappings=[
                     ("/bounding_boxes", "/cone_detection/output"),
                     ("/depth_images", "/zed/zed_node/depth/depth_registered"),
                     ("/camera_pose", "/orb_slam3/camera_pose"),
                     ("/cones", "/slam/cone_map"),
                     ("/camera_info", "/zed/zed_node/depth/camera_info"),
-                    ("/cone_map", "/slam/cone_map")
+                    ("/cone_map", "/slam/cone_map"),
+                    ("/camera_left_image", "/zed/zed_node/left/color/rect/image"),
                 ],
-                parameters=[config, {"debug": LaunchConfiguration("debug")}]
+                parameters=[config, {"debug": LaunchConfiguration("debug")}],
             ),
             ComposableNode(
-                package='foxglove_bridge',
-                plugin='foxglove_bridge::FoxgloveBridge',
-                name='foxglove_bridge',
-            )
+                package="foxglove_bridge",
+                plugin="foxglove_bridge::FoxgloveBridge",
+                name="foxglove_bridge",
+            ),
         ],
-        output = 'both'
+        output="both",
     )
     return LaunchDescription([debug, container])
