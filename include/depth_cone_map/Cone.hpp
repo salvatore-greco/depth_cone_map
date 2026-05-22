b375d1f3e1232a11afcaea627097c3a41952c004
salvatore.greco <greco.salvatore03@gmail.com>
2026-05-22 12:18:48 +0000

    feat: added ConeColor enum

--- include/depth_cone_map/Cone.hpp
+++ include/depth_cone_map/Cone.hpp
@@ -2,10 +2,20 @@
 #define CONE_STRUCT
 
 #include <opencv2/core/types.hpp>
+#include <vector>
+
+enum class ConeColor{
+    UNKNOWN = -1,
+    BLUE,
+    YELLOW,
+    ORANGE,
+    LARGE_ORANGE
+};
+
 struct Cone{
-    Cone(cv::Point3f pos, const std::string& color, int id) : position_world_frame(pos), color(color), id(id){};
+    Cone(cv::Point3f pos, ConeColor color, int id) : position_world_frame(pos), color(color), id(id){};
     cv::Point3f position_world_frame;
-    std::string color;
+    ConeColor color;
     int id;
 };
 
